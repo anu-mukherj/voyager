29fa7bb4ed7662203c4396843bbe05910db36c78
Anwesha <anu.mukherj92@gmail.com>
2018-03-11 19:19:42 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [
+    'uses' => 'IndexController@index',
+    'as' => 'index.index'
+]);
+
+Route::get('/home', [
+    'uses' => 'IndexController@home',
+    'as' => 'index.home'
+]);
+
+Route::get('/post/{post}', [
+    'uses' => 'IndexController@post',
+    'as' => 'index.post'
+]);
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
